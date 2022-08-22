126ae7dd308b92931822b2dbb1ac7ce6094152af
tingold <hutch@earthrise.media>
2022-08-22 13:44:22 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tingold/bun-postgis
+
+go 1.19
+
+require github.com/paulmach/orb v0.7.1
---
+++ postgis.go
@@ -0,0 +1,47 @@
+package bunpostgis
+
+import (
+	"database/sql/driver"
+	"encoding/hex"
+	"fmt"
+	"reflect"
+
+	"github.com/paulmach/orb"
+	"github.com/paulmach/orb/encoding/ewkb"
+)
+
+type PostgisGeometry struct {
+	orb.Geometry
+	SRID int
+}
+
+func (g *PostgisGeometry) Scan(value interface{}) error {
+
+	if value == nil {
+		return nil
+	}
+	var data []byte
+	var err error
+	if reflect.TypeOf(value).Kind() == reflect.String {
+		data, err = hex.DecodeString(value.(string))
+		if err != nil {
+			return err
+		}
+	} else {
+		//need to throw some error here
+		return fmt.Errorf("expected string but got %s", reflect.TypeOf(value).Kind().String())
+	}
+
+	g.Geometry, g.SRID, err = ewkb.Unmarshal(data)
+	return err
+}
+
+func (g *PostgisGeometry) Value() (driver.Value, error) {
+
+	if g.Geometry == nil {
+		return nil, nil
+	}
+	d := ewkb.MustMarshalToHex(g.Geometry, g.SRID)
+	return d, nil
+
+}
